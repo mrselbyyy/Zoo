e0ff65415d8b009969730156a0fc1721979b1935
MrSelby <goodletter@list.ru>
2021-09-13 20:00:49 +0300

    Create ZooPark

---
+++ src/Pack/Animals.java
@@ -0,0 +1,22 @@
+package Pack;
+
+public interface Animals {
+    String getName();
+    double getRealAge();
+    String getMeal();
+
+    public static void main(String[] args) {
+        Lion alex = new Lion("alex", 5,"Meat");
+        System.out.println("Name: " + alex.getName());
+        System.out.println("Ages: " + alex.getRealAge());
+        System.out.println("Meal: " + alex.getMeal());
+        Zebra Morty = new Zebra("Morty", 5, "Green");
+        System.out.println("Name: " + Morty.getName());
+        System.out.println("Ages: " + Morty.getRealAge());
+        System.out.println("Meal: " + Morty.getMeal());
+        Giraffe Malmon = new Giraffe("Malmon", 7,"Trees");
+        System.out.println("Name: " + Malmon.getName());
+        System.out.println("Ages: " + Malmon.getRealAge());
+        System.out.println("Meal: " + Malmon.getMeal());
+    }
+}
---
+++ src/Pack/Hunterble.java
@@ -0,0 +1,5 @@
+package Pack;
+
+public interface Hunterble extends Animals {
+    String hunt();
+}
